765f7500df3139d5cf0f848908844265b2c51f92
Shrinivas Kopparam Ramanath <shrini@uw.edu>
2014-10-24 15:35:15 -0700

    loaded states onto the page

--- js/signup.js
+++ js/signup.js
@@ -2,3 +2,17 @@
     Signup Form Script
     This script will load the state select list and validate the form before submission
 */
+
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+    for(var i = 0; i < usStates.length; i++) {
+        var x = document.createElement("OPTION");
+        x.value = usStates[i].code;
+        x.text = usStates[i].name;
+        document.getElementById("state").appendChild(x);
+    }
+});
+
+document.getElementById("myBtn").addEventListener("click", displayDate);
