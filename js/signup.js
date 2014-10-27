545b53d79c0e2cbf1008571078946079c138be50
Shrinivas Kopparam Ramanath <shrini@uw.edu>
2014-10-27 00:28:26 -0700

    Kind of Works

--- js/signup.js
+++ js/signup.js
@@ -5,17 +5,16 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     var form = document.forms['signup'];
-    var stateList = form.state;
     for (var idx = 0; idx < usStates.length; idx++) {
         var state = usStates[idx];
         var option = document.createElement("OPTION");
         option.value = state.code;
         option.text = state.name;
-        stateList.appendChild(option);
+        form.state.appendChild(option);
     }
 
     var occupation = form.occupation;
-    form.occupation.addEventListener('change', function() {
+    occupation.addEventListener('change', function() {
         var occupationElement = occupation.value;
         if(occupationElement == "other") {
             form.occupationOther.style.display = "block";
@@ -38,33 +37,33 @@ document.addEventListener('DOMContentLoaded', function () {
         var city = form.city.value;
         var state = form.state.value;
         var zip = form.zip.value;
-        var currentOccupation = form.occupation.value;
+        var currentOccupation = occupation.value;
 
-        var validateForm = true;
+        var valid = true;
 
         if(firstName.trim().length == 0) {
-            validateForm = false;
+            valid = false;
             form.firstName.className = 'form-control invalid';
         } else {
             form.firstName.className = 'form-control';
         }
 
         if(lastName.trim().length == 0) {
-            validateForm = false;
+            valid = false;
             form.lastName.className = 'form-control invalid';
         } else {
             form.lastName.className = 'form-control';
         }
 
         if(address1.trim().length == 0) {
-            validateForm = false;
+            valid = false;
             form.address1.className = 'form-control invalid';
         } else {
             form.address1.className = 'form-control';
         }
 
         if(city.trim().length == 0) {
-            validateForm = false;
+            valid = false;
             form.city.className = 'form-control invalid';
         } else {
             form.city.className = 'form-control';
@@ -72,23 +71,23 @@ document.addEventListener('DOMContentLoaded', function () {
 
         var testZipCode = new RegExp('^\\d{5}$');
         if(!testZipCode.test(zip)) {
-            validateForm = false;
+            valid = false;
             form.zip.className = 'form-control invalid';
         } else {
             form.zip.className = 'form-control';
         }
 
         if(!currentOccupation) {
-            validateForm = false;
-            form.occupation.className = 'form-control invalid';
+            valid = false;
+            occupation.className = 'form-control invalid';
         } else {
-            form.occupation.className = 'form-control';
+            occupation.className = 'form-control';
         }
 
         if (currentOccupation == "other") {
             var currentOccupationOther = form.occupationOther.value;
             if(currentOccupationOther.trim().length == 0) {
-                validateForm = false;
+                valid = false;
                 form.occupationOther.className = 'form-control invalid';
             } else {
                 form.occupationOther.className = 'form-control';
@@ -96,7 +95,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         if(!state) {
-            validateForm = false;
+            valid = false;
             form.state.className = 'form-control invalid';
         } else {
             form.state.className = 'form-control';
@@ -109,16 +108,16 @@ document.addEventListener('DOMContentLoaded', function () {
                 document.getElementById('birthdateMessage').innerHTML = "";
             } else {
                 form.birthdate.className = 'form-control invalid';
-                validateForm = false;
+                valid = false;
                 document.getElementById('birthdateMessage').innerHTML = "You are not 13 years old.";
             }
         } else {
             form.birthdate.className = 'form-control invalid';
-            validateForm = false;
+            valid = false;
         }
 
         console.log(age);
-        if(!validateForm) {
+        if(!valid) {
             eventObject.preventDefault();
             eventObject.returnValue = false;
             return false;
